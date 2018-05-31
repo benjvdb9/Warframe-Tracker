c8067d893a95c58bb10f252a2253d448c920a651
Benjamin Vandenbussche <benjvdb9@hotmail.com>
2018-05-31 23:44:49 +0200

    Added report

--- app/src/main/java/com/example/benjvdb/warframetracker/MainScreen.kt
+++ app/src/main/java/com/example/benjvdb/warframetracker/MainScreen.kt
@@ -9,15 +9,15 @@ import android.support.v4.view.GravityCompat
 import android.support.v4.widget.DrawerLayout
 import android.support.v7.app.ActionBar
 import android.support.v7.widget.Toolbar
-import android.util.Log
 import android.view.MenuItem
 import android.widget.Button
 import android.widget.TextView
+import com.beust.klaxon.JsonArray
+import com.beust.klaxon.JsonObject
 import com.beust.klaxon.Parser
 import okhttp3.OkHttpClient
 import okhttp3.Request
 import org.jetbrains.anko.doAsync
-import org.json.JSONObject
 
 class MainScreen : AppCompatActivity (), NavigationView.OnNavigationItemSelectedListener {
 
@@ -58,10 +58,26 @@ class MainScreen : AppCompatActivity (), NavigationView.OnNavigationItemSelected
             var response = client.newCall(request).execute()
             var warframe_body = response.body()!!.string()
             val stringbuilder : StringBuilder = StringBuilder(warframe_body)
-            var warframe_dict = Parser().parse(stringbuilder) as JSONObject
+            var warframe_dict = Parser().parse(stringbuilder) as JsonObject
 
-            var test = "HELLO"
-            Log.d("Body", warframe_dict["Version"].toString())
+            var alerts = warframe_dict["Alerts"] as JsonArray<*>
+            var alert1 = alerts[0] as JsonObject
+            var missioninfo = alert1["MissionInfo"] as JsonObject
+            val minLVL = missioninfo["minEnemyLevel"] as JsonObject
+            val maxLVL = missioninfo["maxEnemyLevel"]
+            val rewards= missioninfo["missionReward"] as JsonObject
+            var itemRewards = "Nothing"
+
+            try {
+                var itemRewards = rewards["items"]
+            } catch(e: Error) {}
+            try {
+                var itemRewards2 = rewards["countedItems"] as JsonArray<*>
+            } catch (e: Error) {}
+
+            val scrollView : TextView = findViewById(R.id.hs_Event_TextView)
+            var text = "A: $minLVL-$maxLVL, $itemRewards\n\n"
+            scrollView.setText(text)
         }
     }
 
